5274f466fc42965b86a50cbe69ae72b680348e18
Vinod Ramdin <digikettechadmin@digikettech.com>
2025-05-01 12:59:22 -0500

    added in changes to the imports syntax

--- src/layouts/dashboard/index.jsx
+++ src/layouts/dashboard/index.jsx
@@ -22,8 +22,11 @@ import MDBox from "../../components/MDBox";
 
 
 // Material Dashboard 2 React example components
-import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
-import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+//import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+//import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import DashboardLayout from "../../examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "../../examples/Navbars/DashboardNavbar";
+
 
 import Footer from "examples/Footer";
 import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
